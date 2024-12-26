286afa89eafac679208fc5b2274633d8858095c2
7klu <debianlunar.sh@gmail.com>
2024-12-26 14:31:42 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-wget $DURL -O minecraft_server_1.21.4.jar
+DURL=$(curl -s https://piston-meta.mojang.com/v1/packages/a3bcba436caa849622fd7e1e5b89489ed6c9ac63/1.21.4.json | jq -r '.downloads.server.url')
 wget $DURL -O minecraft_server_1.21.4.jar
