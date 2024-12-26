541c781e4ee8c990648877777d3a9477e69cb2fa
7klu <debianlunar.sh@gmail.com>
2024-12-26 14:36:25 +0100

    Delete install.sh

--- install.sh
+++
@@ -1,2 +0,0 @@
-DURL=$(curl -s https://piston-meta.mojang.com/v1/packages/a3bcba436caa849622fd7e1e5b89489ed6c9ac63/1.21.4.json | jq -r '.downloads.server.url')
-wget $DURL -O minecraft_server_1.21.4.jar
