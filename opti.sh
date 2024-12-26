0c2c145e7eca2c425da7731340363e76580f62db
7klu <debianlunar.sh@gmail.com>
2024-12-26 15:40:14 +0100

    Update opti.sh

--- opti.sh
+++ opti.sh
@@ -22,24 +22,6 @@ logerror() {
     echo -e "${RED}[ERROR] $1${RESET}"
 }
 
-echo -e "${CYAN}
-o                     __...__     *               
-              *   .--'    __.=-.             o
-     |          ./     .-'     
-    -O-        /      /   
-     |        /    '\"/               *
-             |     (@)     
-            |        \                         
-            |         \ *          |   
-             |  .   /  `                 -O-
-              \  \`~~\                     |
-         o     \     \            *         
-                `\    `-.__           .  
-    .             `--._    `--'   
-                       `---~~`                *
-            *                   o
-${RESET}"
-
 loginfo "Starting Linux Optimizer setup..."
 
 loginfo "Cloning Linux Optimizer repository..."
